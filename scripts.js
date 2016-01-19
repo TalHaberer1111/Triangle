230f803552b3e145f4cc8b4e759c50c5111e1e38
Tal Haberer and Wren Withers <talhaberer2014@gmail.com>
2016-01-19 13:33:03 -0800

    add js functions

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 var triAngle = function(sideA, sideB, sideC) {
   if ( sideA === sideB && sideB === sideC ) {
-    return true;
-    } else {
-    return false;
-    }
+    return "equilateral";
+  } else if (sideA === sideB || sideB === sideC || sideA === sideC){
+    return "isosceles";
+  } else if ((sideA + sideB)< sideC || (sideA + sideC)<sideB || (sideB+sideC)<sideA){
+    return "This is not a triangle.";
+  } else {
+    return "scalene";
+  }
 };
--- specs.js
+++ specs.js
@@ -1,9 +1,18 @@
 describe('triAngle', function() {
   it("is false for three sides where the sum of two are not greater than the third", function() {
-    expect(triAngle(2, 1, 7)).to.equal(false)
+    expect(triAngle(2, 1, 7)).to.equal("This is not a triangle.")
   });
 
   it("is true for three sides of equal length", function() {
-    expect(triAngle(5, 5, 5)).to.equal(true)
+    expect(triAngle(5, 5, 5)).to.equal("equilateral")
   });
+
+  it("is true for two sides of equal length", function() {
+    expect(triAngle(5, 5, 7)).to.equal("isosceles")
+  });
+
+  it("is true for three sides of unequal length", function() {
+    expect(triAngle(3, 5, 7)).to.equal("scalene")
+  });
+
 });
